82b0d4c67af8d690cee940e359dfe9ba2a76887f
David Gold <davidgold@hey.com>
2021-01-14 15:51:08 -0500

    Changed url to https

--- shoppies/src/utils/fetch.js
+++ shoppies/src/utils/fetch.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&type=movie&s=`;
+const URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&type=movie&s=`;
 
 const fetch = async (query) => {
   return await axios.get(`${URL}${query}`);
