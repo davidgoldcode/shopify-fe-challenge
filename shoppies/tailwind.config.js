0034dbd530bf8570625f080d8d19299d3f1e3f4f
David Gold <davidgold@hey.com>
2021-01-14 13:40:33 -0500

    First commit. Added in axios, tailwind and cra

---
+++ shoppies/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./src/index.js",
+    "./src/components/Results.js",
+    "./src/components/Saved.js",
+    "./src/components/Search.js",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
